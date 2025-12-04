4c3e02fed2f2e2ee0075a8fab31b17ff9cd0e2ea
Egor Chuprov <ytdrenk@gmail.com>
2025-12-04 21:27:07 +0300

    version 0.1

--- main.go
+++ main.go
@@ -2,8 +2,50 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const (
+	passwordSize = 24
+
+	uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lowercase = "abcdefghijklmnopqrstuvwxyz"
+	numbers   = "0123456789"
+	symbols   = "!@#$%^&*()-_=+[]{};:'\",.<>/?"
 )
 
 func main() {
-	fmt.Println("Init")
+	rand.Seed(time.Now().UnixNano())
+
+	password := PassGen()
+
+	fmt.Println(password)
+
+	fmt.Println(passwordSize)
+	fmt.Println(uppercase)
+	fmt.Println(lowercase)
+	fmt.Println(numbers)
+	fmt.Println(symbols)
+}
+
+func PassGen() string {
+	password := strings.Builder{}
+	password.Grow(passwordSize)
+
+	for range passwordSize {
+		switch rand.Intn(4) {
+		case 0:
+			password.WriteByte(uppercase[rand.Intn(len(uppercase))])
+		case 1:
+			password.WriteByte(lowercase[rand.Intn(len(lowercase))])
+		case 2:
+			password.WriteByte(numbers[rand.Intn(len(numbers))])
+		case 3:
+			password.WriteByte(symbols[rand.Intn(len(symbols))])
+		}
+	}
+
+	return password.String()
 }
